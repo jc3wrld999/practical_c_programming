ae307e21cf7783ae4aeb49ffc76c83b60c8cb387
jc3wrld999 <jc3wrld999@gmail.com>
2024-10-05 20:46:12 +0900

    add callback function

--- data_structure/circular_list/circular_list.c
+++ data_structure/circular_list/circular_list.c
@@ -5,8 +5,6 @@
 
 List *createList() {
   List *pList = (List *)malloc(sizeof(List));
-  if(pList == NULL)
-    return NULL;
 
   memset(pList, 0, sizeof(List));
   pList->size = 0;
@@ -22,24 +20,6 @@ void deleteList(List *pList) {
 }
 
 int addElement(List *pList, int position, int data) {
-  if(pList == NULL)
-    return FALSE;
-  if(position < 0 || position > getListLength(pList))
-    return FALSE;
-
-  ListNode *pNewNode = (ListNode *)malloc(sizeof(ListNode));
-  if(pNewNode == NULL)
-    return FALSE;
-  pNewNode->data = data;
-
-  ListNode *pPreNode = NULL, *pLastNode;
-
-  if(position == 0) {
-    if(getListLength(pList) == 0) {
-      pList->pLink = pNewNode;
-      pNewNode->pLink = pNewNode;
-    } else {
-      pLastNode = pList->pLink;
-      while (pLastNode->pLink != pList->pLink) n{
-          pLastNode = pLastNode
+  return 0;
+}
 
--- python-list/callback.c
+++ python-list/callback.c
@@ -44,15 +44,89 @@ void my_callback_print(Callback *c)
 }
 
 
-int main(void)
-{
-    Callback *c = callback_create(10);
+// int main(void)
+// {
+//     Callback *c = callback_create(10);
+//
+//     callback_register_print(c, my_callback_print);
+//     callback_print(c);
+//
+//     callback_unregister_print(c);
+//     callback_print(c);
+//
+//     return 0;
+// }
 
-    callback_register_print(c, my_callback_print);
-    callback_print(c);
+#define COMMAND_ARGS    int data, int data2, int data3
+#define CALLBACK_PRINT_FORMAT   "%s() called! data = %d, data2 = %d, data3 = %d\n"
+#define CALLBACK_PRINT_ARGS    data, data2, data3
 
-    callback_unregister_print(c);
-    callback_print(c);
+#ifdef True
+#undef True
+#undef False
+#endif
+
+#ifndef True
+    #define True 1
+    #define False !True
+#endif
+
+typedef struct _command {
+    char *command;
+    void (*func)(COMMAND_ARGS);
+} Command;
+
+void connect_function(COMMAND_ARGS) {
+    printf(CALLBACK_PRINT_FORMAT, __func__, CALLBACK_PRINT_ARGS);
+}
+
+void open_function(COMMAND_ARGS) {
+    printf(CALLBACK_PRINT_FORMAT, __func__, CALLBACK_PRINT_ARGS);
+}
+
+void load_function(COMMAND_ARGS) {
+    printf(CALLBACK_PRINT_FORMAT, __func__, CALLBACK_PRINT_ARGS);
+}
+
+void save_function(COMMAND_ARGS) {
+    printf(CALLBACK_PRINT_FORMAT, __func__, CALLBACK_PRINT_ARGS);
+}
+
+void close_function(COMMAND_ARGS) {
+    printf(CALLBACK_PRINT_FORMAT, __func__, CALLBACK_PRINT_ARGS);
+}
+
+int main(void) {
+    // char *(*)(int, int, char *) (*callback)(int, int, char *);
+    char *(*(*callback)(int))(int, int, char *);
+
+    Command commands[] = {
+        {"connect", connect_function},
+        {"open", open_function},
+        {"load", load_function},
+        {"save", save_function},
+        {"close", close_function},
+    };
+    int command_length = sizeof(commands) / sizeof(Command);
+
+    char command[100];
+    int data, data2, data3;
+
+    char message[1024];
+    fgets(message, sizeof(message), stdin);
+    printf("'%s'\n", message);
+
+    sscanf(message, "%s %d %d %d", command, &data, &data2, &data3);
+
+
+    for(int i = 0; i < command_length; i++) {
+        if(strcmp(commands[i].command, command) == 0) {
+            commands[i].func(CALLBACK_PRINT_ARGS);
+            break;
+        }
+    }
+
+
 
     return 0;
 }
